1e925867e1b013edaf11b70fab7ee7e40f1f5083
Ervin543 <ervindasratt@gmail.com>
2023-05-03 19:08:58 -0500

    added contents to server.js

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.json());
+
+mongoose.connect('mongodb://localhost/social-network', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+  useCreateIndex: true
+});
+
+const db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+app.listen(3000, () => {
+  console.log('Server started on port 3000');
+});
